21e232b91ca3fad0ca65d2ea23faec00373c3fee
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-07-19 13:08:49 -0400

    rendering is good

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 
+
 export default function StarWarsCard(props) {
   return (
-    <Card>
+    <Card className='ui centered class'>
       <Card.Content >
-        <Card.Header>Name: {props.name}</Card.Header>
-        <Card.Meta>Birth Year: {props.birth_year}</Card.Meta>
+        <Card.Header> {props.name}</Card.Header>
+        <Card.Meta color='black'>Birth Year: {props.birthyear}</Card.Meta>
         <Card.Description>
         Gender: {props.gender}
         </Card.Description>
         <Card.Description>
          Height: {props.height}cm
+        </Card.Description>
+        <Card.Description>
+         Weight: {props.mass}cm
+        </Card.Description>
+        <Card.Description>
+         Hair Color: {props.haircolor}
         </Card.Description>
       </Card.Content>
     </Card>
--- starwars/src/components/StarWarsHolder.js
+++ starwars/src/components/StarWarsHolder.js
@@ -1,40 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import StarWarsCard from './StarWarsCard';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import StarWarsCard from "./StarWarsCard";
+import { Button } from "semantic-ui-react";
+import axios from "axios";
 
+export default function StarWarsContainer() {
+  const [data, setData] = useState({});
+  const [character, setCharacter] = useState("1");
 
-export default function StarWarsContainer(){
-    const [data, setData] = useState({});
-    const [character, setCharacter] = useState('1')
+  useEffect(() => {
+    axios.get(`https://swapi.co/api/people/${character}/`).then(res => {
+      const data = res.data;
+      console.log("GUUD", data);
+      setData(res.data);
+    });
+  }, [character]);
 
-    useEffect(() => {
-        axios.get(`https://swapi.co/api/people/${character}/`)
-        .then(res => {
-            const data = res.data;
-            console.log('GUUD', data);
-            setData(res.data);
-        });
-    }, [character]);
-
-    return(
-        <div>
-              <button onClick={() => setCharacter('1')}>Luke Skywalker</button>
-              <button onClick={() => setCharacter('2')}>C-3P0</button>
-              <button onClick={() => setCharacter('3')}>R2-D2</button>
-              <button onClick={() => setCharacter('4')}>Darth Vader</button>
-              <button onClick={() => setCharacter('5')}> Leia Organa</button>
-              <button onClick={() => setCharacter('20')}>Yoda</button>
-              <button onClick={() => setCharacter('21')}>Palpatine</button>
-              <button onClick={() => setCharacter('16')}>Jabba Desilijic Tiure</button>
-              <button onClick={() => setCharacter('13')}>Chewbacca</button>
-              <button onClick={() => setCharacter('11')}>Anakin Skywalker</button>
-              <button onClick={() => setCharacter('22')}>Boba Fett</button>
-                {data ? <StarWarsCard name={data.name}
-                            gender={data.gender}
-                            height={data.height}
-                            birthyear={data.birth_year}
-                             />
-                : <div> Oops one second </div>}
-        </div>
-    )
+  return (
+    <div>
+      <Button basic color='black'onClick={() => setCharacter("1")}>Luke Skywalker</Button>
+      <Button basic color='black' onClick={() => setCharacter("2")}>C-3P0</Button>
+      <Button basic color='black'onClick={() => setCharacter("3")}>R2-D2</Button>
+      <Button basic color='black' onClick={() => setCharacter("4")}>Darth Vader</Button>
+      <Button basic color='black' onClick={() => setCharacter("5")}> Leia Organa</Button>
+      <Button basic color='black'onClick={() => setCharacter("20")}>Yoda</Button>
+      <Button basic color='black'onClick={() => setCharacter("21")}>Palpatine</Button>
+      <Button basic color='black' onClick={() => setCharacter("16")}>Jabba Desilijic </Button>
+      <Button basic color='black'onClick={() => setCharacter("13")}>Chewbacca</Button>
+      <Button basic color='black' onClick={() => setCharacter("11")}>Anakin Skywalker</Button>
+      <Button basic color='black'onClick={() => setCharacter("22")}>Boba Fett</Button>
+      {data ? (
+        <StarWarsCard
+          name={data.name}
+          gender={data.gender}
+          height={data.height}
+          birthyear={data.birth_year}
+          haircolor={data.hair_color}
+        />
+      ) : (
+        <div> Oops one second </div>
+      )}
+    </div>
+  );
 }
